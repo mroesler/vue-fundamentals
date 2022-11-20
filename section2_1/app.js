b040686e939083307c4fbf35d5993de39032e555
Micha Roesler <micha.roesler@gmail.com>
2022-11-20 12:52:25 +0100

    Two way data binding

--- section2_1/app.js
+++ section2_1/app.js
@@ -1,7 +1,30 @@
-Vue.createApp({
+const vm = Vue.createApp({
   data() {
     return {
-      firstName: 'John'
+      firstName: 'John',
+      lastName: 'Doe',
+      getFirstName() {
+        return this.firstName;
+      },
+      setFirstName(name) {
+        this.firstName = name;
+      },
+      getLastName() {
+        return this.lastName;
+      },
+      setLastName(name) {
+        this.lastName = name;
+      }
+    }
+  },
+  methods: {
+    getFullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`
     }
   }
 }).mount('#app')
+
+setTimeout(() => {
+  vm.setFirstName('Bob');
+  vm.setLastName('Roesler');
+}, 2000)
